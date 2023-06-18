68b70772d8840301e4fd281caf4f4e9a9955c077
petkovic82 <petkovic.ra82.2019@uns.ac.rs>
2023-06-18 19:26:02 +0200

    statrt up edited

--- ReservationService/Program.cs
+++ ReservationService/Program.cs
@@ -1,70 +1,19 @@
-using Grpc.Core;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Hosting.Internal;
-using ReservationService;
-using ReservationService.Configuration;
-using ReservationService.Core;
-using ReservationService.Model;
-using ReservationService.Repository;
-using ReservationService.Services;
+
+using Grpc.Net.Client;
 using Proto2;
-//using Proto1;
+using ReservationService;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddDbContext<ApplicationContext>(optionBuilder => {
-    optionBuilder.UseSqlServer("Server=mssql;Database=Reservation;User Id=sa;Password=Your_password123!");
-    optionBuilder.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
-});
-
-//klijent za 4112
-var channel = new Channel("localhost", 4112, ChannelCredentials.Insecure);
-var client = new UserGrpc.UserGrpcClient(channel);
-builder.Services.AddSingleton(client);
-builder.Services.AddGrpc();
-
-builder.Services.AddScoped<IAccommodationService, AccommodationService>();
-builder.Services.AddScoped<IReservationService, ReservationService.Services.ReservationService>();
-builder.Services.AddScoped<IUserService, ReservationService.Services.UserService>();
-
-var projectConfiguration = new ProjectConfiguration();
-builder.Services.AddSingleton(projectConfiguration);
-
-var app = builder.Build();
-app.UseCors(x => x
-    .AllowAnyOrigin()
-    .AllowAnyMethod()
-    .AllowAnyHeader());
-
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+public class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+        var channel = GrpcChannel.ForAddress("https://localhost:4112"); //zapocni channel
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+
+
 }
-
-using (var scope = app.Services.CreateScope())
-{
-    var dbContext = scope.ServiceProvider.GetService<ApplicationContext>();
-    dbContext.Database.Migrate();
-}
-
-app.UseAuthorization();
-
-app.MapControllers();
-//app.UseEndpoints(endpoints =>
-//{
-//    endpoints.MapGrpcService<AccommodationGrpcService>();
-//});
-//server za 4111
-//var server = new Server { Services = { AccommodationGrpc.BindService(new AccommodationGrpcService()) }, Ports = { new ServerPort("localhost", 4111, ServerCredentials.Insecure) } };
-//server.Start();
-app.Run();
---
+++ ReservationService/Startup.cs
@@ -0,0 +1,121 @@
+using Grpc.Core;
+using Microsoft.EntityFrameworkCore;
+using Proto2;
+using ReservationService.Configuration;
+using ReservationService.Core;
+using ReservationService.Model;
+using ReservationService.Repository;
+using ReservationService.Services;
+
+namespace ReservationService;
+
+public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            //dodato
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+            services.AddCors();
+
+            services.AddGrpc();
+
+
+            services.AddDbContext<ApplicationContext>(optionBuilder => {
+                optionBuilder.UseSqlServer("Data Source=DESKTOP-7H680CJ;Initial Catalog=Reservation;Integrated Security=true;");
+                optionBuilder.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+            });
+          //  services.AddScoped<IUserRepository, UserRepository>();
+           
+          services.AddScoped<IAccommodationService, AccommodationService>();
+          services.AddScoped<IReservationService, ReservationService.Services.ReservationService>();
+          services.AddScoped<IUserService, UserService>();
+
+            var config = new ProjectConfiguration();
+            Configuration.Bind("ProjectConfiguration", config);      //  <--- This
+            services.AddSingleton(config);
+            
+            
+            
+            services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
+            {
+                builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+            }));
+        
+        }
+
+        private Server server;
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env,IHostApplicationLifetime applicationLifetime)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            
+            app.UseCors(builder => builder
+        .   AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+            
+            app.UseHttpsRedirection();
+
+            applicationLifetime.ApplicationStopping.Register(() =>
+            {
+                // Shutdown gRPC channel
+                app.ApplicationServices.GetService<Channel>().ShutdownAsync().Wait();
+            });
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+                options.RoutePrefix = string.Empty;
+            });
+            app.UseCors("MyPolicy");
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            using (var sScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                var cx = sScope.ServiceProvider.GetService<ApplicationContext>();
+                cx?.Database.Migrate();
+            }
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+          //      endpoints.MapGrpcService<AccommodationGrpcService>();
+
+            });
+
+        /*    server = new Server
+            {
+                Services = { UserGrpc.BindService(new AccommodationGrpcService()) },
+                Ports = { new ServerPort("localhost", 4112, ServerCredentials.Insecure) }
+            };
+            server.Start();
+*/
+        }
+    }
